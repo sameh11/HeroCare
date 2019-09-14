9f8c56e434e719966d507870974ae87e2be19207
Sameh-Hamza <sameh.hamza11@gmail.com>
2019-09-14 20:55:47 +0200

    Update - Login (Interface, Service, Controller ) New - JwtToken ( Interface, Service, Options )

---
+++ BusinessApp.Core/ApplicationService/IService/ITokenManagerService.cs
@@ -0,0 +1,9 @@
+﻿using BusinessApp.Core.Entity.Users;
+
+namespace BusinessApp.Core.ApplicationService.IService
+{
+    public interface ITokenManagerService
+    {
+        string GenerateJwtToken(object user);
+    }
+}
